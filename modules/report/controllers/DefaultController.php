571770fd3fa3bc1a27e80fed02c3282279a43113
Ruben <rbntejeda@hotmail.com>
2017-06-03 12:38:07 -0400

    install module pdf

--- modules/report/controllers/DefaultController.php
+++ modules/report/controllers/DefaultController.php
@@ -4,17 +4,12 @@ namespace app\modules\report\controllers;
 
 use yii\web\Controller;
 
-/**
- * Default controller for the `report` module
- */
 class DefaultController extends Controller
 {
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
     public function actionIndex()
     {
-        return $this->render('index');
+    	$mpdf = new \mPDF();
+		$mpdf->WriteHTML('Hola Mundo');
+		$mpdf->Output();
     }
 }
