f14d1b7703ab4f9afe8c2d3f7762372565373e13
Ruben <rubentejedaroa@gmail.com>
2017-08-18 11:49:59 -0300

    Endoint refresh and revoke

--- controllers/AuthenticationController.php
+++ controllers/AuthenticationController.php
@@ -49,16 +49,59 @@ class AuthenticationController extends Controller
 
 	public function actionRefresh()
 	{
-        $refresh = Yii::$app->request->post('refresh');
+		$request = Yii::$app->request;
+		// tipo de metodo
+		switch ($request->post('grant_type')) {
+			case 'refresh_token':
+				break;		
+			default:
+                throw new HttpException(405, "Method Not Allowed.");
+				break;
+		}
+		//Busca el cliente
+		$client=Client::findOne(['name'=>$request->post('client_id'),'secret'=>$request->post('client_secret')]);
+		if(empty($client)){
+            throw new HttpException(401, "Error en credenciales. Cliente invalido.");
+		}
+
+        $refresh = Yii::$app->request->post('refresh_token');
         if(empty($refresh)){
             throw new HttpException(401, "Solicitud Invalida.");
         }
-        $auth=Authentication::findActive()->andWhere(['refresh'=>$refresh])->one();
+
+        $auth=Authentication::findActive()->andWhere(['refresh'=>$refresh,'client_id'=>$client->id])->one();
         if(empty($auth)){
             throw new HttpException(401, "Unauthorized.");
         }else{
         	if($auth->Renovate()){
-        		return $auth->refresh;
+        		return [
+        			'refresh'=>$auth->refresh,
+        			'expire_in'=>3600
+        		];
+        	}
+        }
+	}
+
+	public function actionRevoke()
+	{
+		$request = Yii::$app->request;
+		//Busca el cliente
+		$client=Client::findOne(['name'=>$request->post('client_id'),'secret'=>$request->post('client_secret')]);
+		if(empty($client)){
+            throw new HttpException(401, "Error en credenciales. Cliente invalido.");
+		}
+        $token = Yii::$app->request->post('token');
+        if(empty($token)){
+            throw new HttpException(401, "Solicitud Invalida.");
+        }
+
+        $auth=Authentication::findActive()->andWhere(['token'=>$token,'client_id'=>$client->id])->one();
+        if(empty($auth)){
+            throw new HttpException(401, "Unauthorized.");
+        }else{
+        	$auth->status="DENY";
+        	if($auth->save(false,['status'])){
+        		return ['status'=>true];
         	}
         }
 	}
