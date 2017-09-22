356ae23df48ed926e3c40a098ef8d8a9d3de2589
danielgonzalor <danielgonzalor@gmail.com>
2017-09-22 11:25:35 -0300

    models and controller rvclient

---
+++ modules/v1/controllers/TimsController.php
@@ -0,0 +1,140 @@
+<?php 
+
+namespace app\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+use app\modules\v1\models\RvFicha;
+class TimsController extends ActiveController
+{
+	// public function behaviors()
+	// {
+	// 	return \yii\helpers\ArrayHelper::merge(parent::behaviors(),[
+	// 		'authenticator'=>[
+	// 			'class' => \yii\filters\auth\HttpBearerAuth::className()  
+	// 		],
+	// 		'authorization'=>[
+	// 			'class' => \app\components\Authorization::className(),
+	// 		],
+	// 	]);
+	// }
+	public function actions()
+	{
+	    $actions = parent::actions();
+
+	    // disable the "delete" and "create" actions
+	    unset($actions['delete'], $actions['create']);
+	    return $actions;
+	}
+	public $modelClass = 'app\modules\v1\models\RvClientParams';
+
+	
+    public function actionCreate()
+	{
+		 //paramatro de entrada fic_id
+		$post = \Yii::$app->request->post();
+		if(isset( $post['fic_id']) ){
+			$id = $post['fic_id'];
+			$urlInscripcion = "https://timshr.com/pca2/core/api/WS/AddPca";
+			$ficha = RvFicha::find()->andWhere(["fic_id" => $id])->one();
+			$tra = $ficha->trabajador;
+			$sexo = $tra->sexo;
+			switch ($sexo) {
+				case 'MASCULINO':
+				$sexoL = 'M';
+				break;
+				case 'FEMENINO':
+				$sexoL = 'F';
+				break;
+				default:
+				$sexoL = 'M'; 
+				break;
+			}
+			$fields = array(	        
+				'CoKey'=> "5ccf4857-2691-4eaf-b2e0-f7d42375691c",
+				'PerNom'=>  $tra->nombre,
+				'PerPriApe'=> $tra->paterno,
+				'PerSegApe'=> $tra->materno,
+				'PerNumIde'=> $ficha->fic_id,
+				'PerGen'=>  $sexoL,
+				'CoRegCod'=> "es-cl",
+				'PcaTip'=> "D"
+			);
+			$result = $this->curl_post($fields,$urlInscripcion);
+			// $datos->fic_id = '1';
+			// $datos->cli_eva_id = '1'
+			// $datos->type = 'json';
+			// $datos->content =  '';
+
+
+			$model=new $this->modelClass;
+			$model->fic_id='1';
+			$model->save();
+		 
+		return $model;
+
+
+		}else{
+			throw new \yii\web\HttpException(404, 'No existen entradas con los parametros propuestos.');
+		}
+   
+	}
+ 
+ 
+
+	public function actionSearch()
+	{
+		if (!empty($_GET)) {
+			$request=\Yii::$app->request;
+			$reserve=['per-page','sort','page','expand'];
+			$model = new $this->modelClass;
+			foreach ($_GET as $key => $value) {
+				if (!$model->hasAttribute($key)&&!in_array($key,$reserve)) {
+					throw new \yii\web\HttpException(404, 'Atributo invalido :' . $key);
+				}
+			}
+			try {
+			   	$query = $model->find();
+			   	$range=['id'];
+				foreach ($_GET as $key => $value) {
+					if(!in_array($key,$reserve)){
+						if (in_array($key,$range)) {
+							$limit = explode('-',$value);
+							$query->andWhere(['between', $key,$limit[0],$limit[1]]);
+						}else{
+							$query->andWhere(['like', $key, $value]);
+						}
+					}
+				}
+				$provider = new \yii\data\ActiveDataProvider(['query' => $query]);
+			} catch (Exception $ex) {
+				throw new \yii\web\HttpException(500, 'Error interno del sistema.');
+			}
+
+			if ($provider->getCount() <= 0) {
+				throw new \yii\web\HttpException(404, 'No existen entradas con los parametros propuestos.');
+			} else {
+				return $provider;
+			}
+		} else {
+			throw new \yii\web\HttpException(400, 'No se puede crear una query a partir de la informacion propuesta.');
+		}
+	} 
+	private function curl_post($fields,$url){ //codigo que curl que crea la comunicacion via post
+    	$postvars='';
+		$sep='';
+		foreach($fields as $key=>$value) 
+		{
+			$postvars.= $sep.urlencode($key).'='.urlencode($value);
+			$sep='&';
+		}
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_POST,count($fields));
+		curl_setopt($ch,CURLOPT_POSTFIELDS,$postvars);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		$result = json_decode($result);	
+		return $result;
+    }
+}
