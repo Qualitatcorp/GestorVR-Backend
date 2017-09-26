da5cce405082b62eb042c91c696c08b4d6643aa9
danielgonzalor <danielgonzalor@gmail.com>
2017-09-26 16:50:45 -0300

    ignore params

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'BasePathImage'=>'http://localhost/GestorVR-WEB/images/',
+    'BasePathImage'=>'http://gestorvr.qualitatcorp.cl/images/',
     'baseUrlFront'=>'http://localhost/src/',
     'baseDirFront'=>"C:/xampp/htdocs/src/"
 ];
