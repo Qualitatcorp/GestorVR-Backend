7167426ca0f02e5fdc3a693868dce6af86af72b4
Ruben <rbntejeda@hotmail.com>
2017-10-01 11:44:23 -0300

    path Image

--- config/params.php
+++ config/params.php
@@ -3,7 +3,6 @@
 return [
     'adminEmail' => 'admin@example.com',
     'BasePathImage'=>'http://gestorvr.qualitatcorp.cl/images/',
-    'BasePathImage'=>'http://127.0.0.1/images/',
     'baseUrlFront'=>'http://localhost/src/',
     'baseDirFront'=>"d:/usuario/xampp/htdocs/src/"
 ];
